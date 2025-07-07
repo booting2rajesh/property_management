4cc8bc5f209b4e0551b0fb672b41c6e22002be63
booting2rajesh <155865605+booting2rajesh@users.noreply.github.com>
2025-07-07 07:57:05 +0530

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/  https://github.com/booting2rajesh/property_management/", // ← ADD THIS LINE
   server: {
     host: "::",
     port: 8080,
